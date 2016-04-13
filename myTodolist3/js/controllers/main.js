c2c10b81d4524be5d12cf930bc4eaba75165456f
xiguatailangmaixigua <574020135@qq.com>
2016-04-13 16:31:38 +0800

    Added files via upload

--- myTodolist3/js/controllers/main.js
+++ myTodolist3/js/controllers/main.js
@@ -2,102 +2,103 @@
 'use strict';
  angular.module('mytodoApp',[])
  .controller('MainCtrl',function($scope){
- 	$scope.todos=[];
- 	$scope.active=[];
- 	$scope.completed=[];
- 	$scope.len=0;
- 	$scope.allFlag=true;
- 	$scope.activeFlag=false;
- 	$scope.completeFlag=false;
- 	$scope.doneAll=false;
- 	//add todo
- 	$scope.addTodo = function(){
- 			$scope.doneAll=false;
- 			$scope.todos.push({
- 			content:$scope.todo,
+ 	$scope.todoSet = [];
+ 	$scope.activeSet = [];
+ 	$scope.completedSet = [];
+ 	$scope.activeLen = 0;
+ 	$scope.doneAll = false;
+ 	$scope.type = 'all';
+
+ 	$scope.addTodo = function() {
+
+ 		$scope.doneAll = false;
+ 		$scope.todoSet.push({
+ 			content: $scope.todo,
  			isDone: false
  		});
- 			$scope.active.push($scope.todo);
-	 		$scope.todo='';
-	 		$scope.len=$scope.active.length;
+ 			$scope.activeSet.push($scope.todo);
+	 		$scope.todo = '';
+	 		$scope.activeLen = $scope.activeSet.length;
  	};
- 	//remove a todo ,used when click remove button in todos list 
- 	$scope.removeTodo1= function(index)
- 	{
- 		$scope.active.splice($scope.active.indexOf($scope.todos[index].content),1);
-   		$scope.todos.splice(index,1);
-   		$scope.len=$scope.active.length;
+
+ 	$scope.removeTodo = function(event) {  
+ 	
+ 		var deleteNum = event.target.parentNode.parentNode.firstElementChild.firstChild.nodeValue;
+   		var i;
+   		for (i=0;i<$scope.todoSet.length;i++) {
+   			if ($scope.todoSet[i].content==deleteNum) {
+   				$scope.todoSet.splice($scope.todoSet[i],1);
+   				break;
+   			}
+   		}
+   		$scope.activeSet.splice($scope.activeSet.indexOf(deleteNum),1);
+   		$scope.completedSet.splice($scope.completedSet.indexOf(deleteNum),1);
+   		$scope.activeLen = $scope.activeLen-1;
+ 	};
+ 	
+ 	$scope.completeTodo = function(event,status) {
+
+ 		var deleteNum = event.target.parentNode.parentNode.firstElementChild.firstChild.nodeValue;
+   		var i;
+
+   		$scope.activeSet = [];
+   		$scope.completedSet = [];
+    
+   		if (status == 'active') {
+   			for (i=0;i<$scope.todoSet.length;i++) {
+   				if ($scope.todoSet[i].content == deleteNum) {
+   					$scope.todoSet[i].isDone = true;
+   					break;
+   				}
+   			}
+   		}
+  
+   		for (i=0;i<$scope.todoSet.length;i++) {
+   			if (!$scope.todoSet[i].isDone){
+   				$scope.activeSet.push($scope.todoSet[i].content);		
+   			}
+   			else {
+   				$scope.completedSet.push($scope.todoSet[i].content);
+   			}
+   		}
+   		$scope.activeLen = $scope.activeSet.length;
+ 	};
+
+ 	$scope.changeListType = function(thisType) {
+
+ 		$scope.type = thisType;
  	}
- 	//complete a todo ,used when click the complete button in todo list
- 	$scope.completeTodo1=function(index,element){
- 		if($scope.todos[index].isDone){
- 			$scope.completed.push($scope.todos[index].content);
- 			$scope.active.splice($scope.active.indexOf($scope.todos[index].content),1);
- 			$scope.len=$scope.active.length;	
- 		}
- 		else{
- 			$scope.active.push($scope.todos[index].content);
- 			$scope.completed.splice($scope.completed.indexOf($scope.todos[index].content),1);
- 			$scope.len=$scope.active.length;
- 		}
+
+ 	$scope.clearComplete = function() {
+
+ 		var i;
+
+ 		for (i=0;i<$scope.todoSet.length;i++) {
+   			if ($scope.todoSet[i].isDone){
+   				$scope.todoSet.splice(i,1);		
+   			}
+   		}
+ 		$scope.completedSet=[];
  	}
- 	//remove a todoo ,used when click the remove button in ative list
- 	 $scope.removeTodo2= function(index)
- 	{	
-		for(var i=0;i<$scope.active.length;i++){
-			if($scope.todos[i].content==$scope.active[index]){
-				$scope.todos.splice(i,1);
-				break;
-			}
-		}
-   		$scope.active.splice(index,1);
-   		$scope.len=$scope.active.length;
- 	}
- 	//complete a todo when click the  complete button in active list
- 	$scope.completeTodo2=function(index){
- 			var j=0;
- 			$scope.completed.push($scope.active[index]);
- 			$scope.active.splice(index,1);
- 			$scope.len=$scope.active.length;
- 	}
- 	$scope.showAll=function(){
- 		$scope.allFlag=true;
-	 	$scope.activeFlag=false;
-	 	$scope.completeFlag=false;
- 	}
- 	$scope.showActive=function(){
- 		$scope.allFlag=false;
-	 	$scope.activeFlag=true;
-	 	$scope.completeFlag=false;
-	 	$scope.doneAll=false;
- 	}
- 	$scope.showComplete=function(){
- 		$scope.allFlag=false;
-	 	$scope.activeFlag=false;
-	 	$scope.completeFlag=true;
-	 	$scope.doneAll=false;
- 	}
- 	$scope.clearComplete= function(){
- 		$scope.completed=[];
- 	}
- 	//choose all as completed
+ 	//choose all as completedSet
  	$scope.doneAllTodo = function(){
- 		if($scope.doneAll){
-	 		for(var i=0;i< $scope.todos.length;i++){
-	 			$scope.todos[i].isDone=true;
-	 			$scope.completed.push($scope.todos[i].content);
+
+ 		if ($scope.doneAll) {
+	 		for (var i=0;i< $scope.todoSet.length;i++) {
+	 			$scope.todoSet[i].isDone = true;
+	 			$scope.completedSet.push($scope.todoSet[i].content);
 	 		}
-	 		$scope.len=0;
-	 		$scope.active=[];
+	 		$scope.activeLen = 0;
+	 		$scope.activeSet = [];
  		}
- 		else{
- 			for(var i=0;i< $scope.todos.length;i++){
-	 			$scope.todos[i].isDone=false;
-	 			$scope.active.push($scope.todos[i].content);
+ 		else {
+ 			for (var i=0;i< $scope.todoSet.length;i++) {
+	 			$scope.todoSet[i].isDone = false;
+	 			$scope.activeSet.push($scope.todoSet[i].content);
 	 			
 	 		}
- 			$scope.len=$scope.active.length;
- 			$scope.completed=[];
+ 			$scope.activeLen = $scope.activeSet.length;
+ 			$scope.completedSet = [];
  		}
  	}
  });
